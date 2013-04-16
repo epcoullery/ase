6e1b725f82274b1880ded36876e64ab81574c5bb
alazo <alain.zosso@webzos.com>
2013-04-16 19:33:37 +0200

    Mise en place common/local_settings

--- common/settings.py
+++ common/settings.py
@@ -50,7 +50,7 @@ MEDIA_URL = ''
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'repart/static')
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 
 # URL prefix for static files.
 STATIC_URL = '/static/'
